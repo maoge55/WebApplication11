a16bd5c5afb9652a9b265bb19c90fa24b2c69a8d
maoge55 <1126642524@qq.com>
2025-10-24 14:44:59 +0800

    修改图片代理

--- cg/cjt/ImageProxy.aspx.cs
+++ cg/cjt/ImageProxy.aspx.cs
@@ -12,7 +12,7 @@ namespace WebApplication11.cg.cjt
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string imageUrl = Request.QueryString["url"];
+            string imageUrl = HttpUtility.UrlDecode(Request.QueryString["url"]);
             if (string.IsNullOrWhiteSpace(imageUrl) || !Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
             {
                 Response.StatusCode = 400;
